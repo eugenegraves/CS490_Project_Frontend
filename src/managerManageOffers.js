26cd3cbe0b227cee995ee992ffaddde439894988
eugenegraves <elg22@njit.edu>
2024-05-08 04:16:35 -0400

    Attempt #7

--- src/managerManageOffers.js
+++ src/managerManageOffers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { BrowserRouter as useNavigate } from 'react-router-dom';
 import { Text, Heading, Box, Flex, Grid, Button, Image } from "@chakra-ui/react";
 import axios from 'axios';
